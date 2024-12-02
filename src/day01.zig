4bb63378b478994b341e49bf46a4e98e0a4e1be5
Roberto Esteves <contact@robertoesteves.dev>
2024-12-02 13:35:36 -0300

    day 01

--- src/day01.zig
+++ src/day01.zig
@@ -11,7 +11,49 @@ const gpa = util.gpa;
 const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
-    
+    var tokenizer = tokenizeAny(u8, data, " \n");
+
+    var numbers1 = List(u32).init(gpa);
+    var numbers2 = List(u32).init(gpa);
+
+    while (tokenizer.peek() != null) {
+        const number1 = tokenizer.next().?;
+        const number2 = tokenizer.next().?;
+        try numbers1.append(try parseInt(u32, number1, 10));
+        try numbers2.append(try parseInt(u32, number2, 10));
+    }
+
+    sort(u32, numbers1.items, {}, asc(u32));
+    sort(u32, numbers2.items, {}, asc(u32));
+
+    var sum: u64 = 0;
+    for (0..numbers1.items.len) |i| {
+        if (numbers1.items[i] < numbers2.items[i]) {
+            sum += @abs(numbers2.items[i] - numbers1.items[i]);
+        } else {
+            sum += @abs(numbers1.items[i] - numbers2.items[i]);
+        }
+    }
+    print("{}\n", .{sum});
+
+    var score: u64 = 0;
+    var number_map = Map(u32, u32).init(gpa);
+    for (numbers1.items) |number1| {
+        if (number_map.contains(number1)) {
+            score += number1 * number_map.get(number1).?;
+        } else {
+            var count: u32 = 0;
+            for (numbers2.items) |number2| {
+                if (number2 == number1) {
+                    count += 1;
+                }
+            }
+
+            score += number1 * count;
+            try number_map.put(number1, count);
+        }
+    }
+    print("{}\n", .{score});
 }
 
 // Useful stdlib functions
