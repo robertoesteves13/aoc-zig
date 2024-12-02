232a12a01ae943e2ae4fd80f38659c65f1a56110
Roberto Esteves <contact@robertoesteves.dev>
2024-12-02 19:17:57 -0300

    day 2: failed attempt

--- src/day02.zig
+++ src/day02.zig
@@ -10,8 +10,81 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
+const order = enum { asc, desc, unset };
+
+fn print_list(list: List(i32), distances: List(i32)) void {
+    print("{any}\n", .{list.items});
+    print("{any}\n", .{distances.items});
+    print("\n", .{});
+}
+
 pub fn main() !void {
-    
+    var number_matrix = List(List(i32)).init(gpa);
+
+    var lines = tokenizeAny(u8, data, "\n");
+    while (lines.peek() != null) {
+        var numbers = tokenizeAny(u8, lines.next().?, " ");
+        var list = List(i32).init(gpa);
+        while (numbers.peek() != null) {
+            try list.append(try parseInt(i32, numbers.next().?, 10));
+        }
+        try number_matrix.append(list);
+    }
+
+    var valid_reports: u64 = 0;
+    for (0..number_matrix.items.len) |u| {
+        const list = number_matrix.items[u];
+        var distances = List(i32).init(gpa);
+        var sum: i32 = 0;
+        for (1..list.items.len) |i| {
+            const num = list.items[i - 1] - list.items[i];
+            try distances.append(num);
+            sum += num;
+        }
+
+        if (sum < 0) {
+            for (0..distances.items.len) |i| {
+                distances.items[i] *= -1;
+            }
+            sum *= -1;
+        }
+
+        var errors = List(usize).init(gpa);
+        for (0..distances.items.len) |i| {
+            if (distances.items[i] > 3 or distances.items[i] <= 0) try errors.append(i);
+        }
+
+        if (errors.items.len == 0) {
+            valid_reports += 1;
+        } else {
+            if (errors.items.len == 1) {
+                const idx = errors.items[0];
+                const distance = distances.items[idx];
+
+                if (distance > 0) {
+                    if (idx == 0 or idx == distances.items.len - 1) valid_reports += 1;
+                } else {
+                    if (distance == 0) valid_reports += 1 else {
+                        if (idx == 0 or idx == distances.items.len - 1) valid_reports += 1 else {
+                            const val = distance + distances.items[idx + 1];
+                            if (val <= 3 or val > 0) valid_reports += 1;
+                        }
+                    }
+                }
+            }
+            if (errors.items.len == 2) {
+                const idx_mean = errors.items[1] - errors.items[0];
+                if (idx_mean == 1) {
+                    const val = distances.items[errors.items[1]] + distances.items[errors.items[0]];
+                    if (val <= 3 and val > 0) {
+                        valid_reports += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    print("Valid Reports: {}\n", .{valid_reports});
 }
 
 // Useful stdlib functions
